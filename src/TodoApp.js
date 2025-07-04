9dba2ae11850e65e656e3f0ccbd96b262d5491ca
Lucas Gomes <lulinux2109@gmail.com>
2025-07-03 21:05:01 -0300

    refactor: remove buttonHelp

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -2,7 +2,7 @@ import React from "react";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 import VisibilityFilters from "./components/VisibilityFilters";
-import { ButtonHelp } from "./components/ButtonHelp";
+
 import "./styles.css";
 
 export default function TodoApp() {
@@ -12,14 +12,6 @@ export default function TodoApp() {
       <AddTodo />
       <TodoList />
       <VisibilityFilters />
-      {/* Dentro de Configurações → Usuários */}
-      <ButtonHelp category="configuracoes" article="usuarios" />
-
-      {/* Dentro de Financeiro → Faturas */}
-      <ButtonHelp category="financeiro" article="faturas" />
-
-      {/* Dentro de Financeiro → Reembolsos */}
-      <ButtonHelp category="financeiro" article="reembolsos" />
     </div>
   );
 }
